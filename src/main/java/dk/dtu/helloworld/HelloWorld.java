a0bc8dceb343d98733ff9f5430cecd3356e592a9
HeroGamers <hero@herogamers.dev>
2021-09-20 15:18:56 +0200

    Initial commit

---
+++ src/main/java/dk/dtu/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package dk.dtu.helloworld;
+
+import java.util.Arrays;
+
+// Main class for the Hello, World! program
+public class HelloWorld {
+    // Main function for the Hello, World! class
+    public static void main(String[] args) {
+        // Print Hello, World! out
+        System.out.println("Hello, World!");
+    }
+}
